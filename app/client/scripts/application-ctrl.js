af15cc50c1819de71e6e87a41937a80bd77ba284
bozobjekovic <bozo.bjekovic@gmail.com>
2017-01-27 01:09:20 +0100

    Timeline for errors, fixed some bugs and fixed view

--- app/client/scripts/application-ctrl.js
+++ app/client/scripts/application-ctrl.js
@@ -6,7 +6,10 @@
 		    function($scope, $routeParams, $location, $localStorage, applicationFactory) {
 
 			var param = $routeParams.param;
-			$scope.filter = [];
+			$scope.filter = null;
+			$scope.fragments = [];
+			$scope.timelineEvents = [];
+			$scope.showMainType = true;
 			$scope.sortByTime = 'timeStamp';
 			$scope.sortByVersion = 'version';
 
@@ -20,18 +23,52 @@
 			
 			applicationFactory.getEvents($localStorage.currentUser._id, param).then(function(items) {
 				$scope.events = items;
+
+				$scope.fragments.push('All');
+				$scope.filter = $scope.fragments[0];
+				for (var i = 0; i < items.length; i++) {
+					$scope.fragments.push(items[i].fragment);
+				}
 			});
 			
-			// FIX BUG
 			$scope.optionClick = function(){
-				for (var property in $scope.filter) {
-				    if ($scope.filter.hasOwnProperty(property)) {
-				    	var fragment = $scope.filter[property].fragment;
-						applicationFactory.getEventsByFragment($localStorage.currentUser._id, param, fragment).then(function(items) {
-							$scope.events = items;
-						});	
-				    }
-				}			
+				if ($scope.filter === 'All') { 
+					$scope.showMainType = true;
+
+					applicationFactory.getEvents($localStorage.currentUser._id, param).then(function(items) {
+						$scope.events = items;
+					});
+				} else {
+					$scope.showMainType = false;
+
+					$scope.timelineEvents = [];
+					applicationFactory.getEventsByFragment($localStorage.currentUser._id, param, $scope.filter).then(function(items) {
+						var date = new Date(items[0].timeStamp);
+						date.setHours(0, 0, 0, 0);
+						$scope.timelineEvents.push({
+							date : date,
+							events : [items[0]]
+						});
+						for (var i = 1; i < items.length; i++) {
+							var datee = new Date(items[i].timeStamp);
+							datee.setHours(0, 0, 0, 0);
+							var exist = false;
+							for (var j = 0; j < $scope.timelineEvents.length; j++) {
+								if (datee.getTime() == $scope.timelineEvents[j].date.getTime()) {
+									$scope.timelineEvents[j].events.push(items[i]);
+									exist = true;
+									break;
+								}
+							}
+							if (!exist) {
+								$scope.timelineEvents.push({
+									date : datee,
+									events : [items[i]]
+								});
+							}
+						}
+					});
+				}
 			}
 
             $scope.inviteUser = function() {
